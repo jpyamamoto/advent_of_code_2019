04594f1166ce9d947f7428514b647601355c361c
Juan Pablo Yamamoto <jpyamamoto@ciencias.unam.mx>
2019-12-04 14:05:03 -0600

    Day 04. Part 2.

--- src/Day04/Day04.ts
+++ src/Day04/Day04.ts
@@ -9,17 +9,24 @@ class Day04 extends DaySolution {
     return digits.every((num, i, arr) => i == 0 ? true : arr[i-1] <= num);
   }
 
+  private notThreeEqualsDigits(num: string): boolean {
+    const digits = num.split("").map(num => Number.parseInt(num));
+    return digits.some((num, i, arr) => (i < 5) && (num == arr[i+1])
+                                        && (arr[i-1] != num)
+                                        && (arr[i+1] != arr[i+2]));
+  }
+
   private adjacentDoubleDigits(num: string): boolean {
     const digits = num.split("").map(num => Number.parseInt(num));
     return digits.some((num, i, arr) => arr[i-1] == num);
   }
 
-  private bruteForceEvaluation(): number[] {
+  private bruteForceEvaluation(criteria: ((s: string) => boolean)[]): number[] {
     const result: number[] = [];
 
     for (let num = this.INPUT_MIN; num <= this.INPUT_MAX; num++) {
       const numString = num.toString();
-      if (this.increasingDigits(numString) && this.adjacentDoubleDigits(numString)) {
+      if (criteria.every(criterion => criterion(numString))) {
         result.push(num);
       }
     }
@@ -28,12 +35,17 @@ class Day04 extends DaySolution {
   }
 
   runSolution1 (): string {
-    const result = this.bruteForceEvaluation();
+    const result = this.bruteForceEvaluation(
+      [this.increasingDigits, this.adjacentDoubleDigits]
+    );
     return result.length.toString();
   }
 
   runSolution2 (): string {
-    return "Not yet implemented.";
+    const result = this.bruteForceEvaluation(
+      [this.increasingDigits, this.notThreeEqualsDigits, this.adjacentDoubleDigits]
+    );
+    return result.length.toString();
   }
 }
 
